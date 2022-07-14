9f4030f0ff6e8dea7783522ffec4b44c1d7a9179
John3107 <maynsayt@gmail.com>
2022-07-14 19:28:51 +0300

    Add npm deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import React, {useEffect} from 'react';
 import style from './App.module.scss';
 import {getUsersTC} from "./app-reducer";
 import {useAppDispatch} from "../hooks/hooks";
-import {AppRouting} from "../routing/appRouting";
 import {Route, Routes} from "react-router-dom";
 import {MainPage} from "../components/MainPage/MainPage";
 import {UserPage} from "../components/UserPage/UserPage";
@@ -17,7 +16,6 @@ function App() {
 
     return (
         <div className={style.app}>
-            {/*<AppRouting/>*/}
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
                 <Route path="/user/:ghLogin" element={<UserPage/>}/>
